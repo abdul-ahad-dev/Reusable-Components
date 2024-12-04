156ac132c50a22e700c2efebfc4a597ef88b4fcc
abdulahad0110 <abdulahad200325@gmail.com>
2024-12-05 03:57:26 +0500

    first commit

--- app/themeChanger.tsx
+++ app/themeChanger.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { View } from 'react-native'
 import { ThemeButton } from '@/components/ThemedButton';
 import { ThemedText } from '@/components/ThemedText';
-import { Theme } from '@react-navigation/native';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/store/store';
 
 
 export default function themeChanger() {
+  const theme = useSelector((state: RootState) => state.theme.theme);
+  const dispatch = useDispatch();
+  console.log('first==>',theme);
+
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', gap: 12 }} >
-      <ThemedText type='title' align='center'>Change Theme</ThemedText>
+      <ThemedText type='title' align='center' style={{backgroundColor: '#000'}}>Change Theme</ThemedText>
       <ThemeButton mx={10} txt="Light" />
       <ThemeButton mx={10} txt="Dark" />
     </View>
