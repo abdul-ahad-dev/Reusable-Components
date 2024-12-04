31afd0dd87dbb93c79ee33889e481c5ae2e444b4
abdulahad0110 <abdulahad200325@gmail.com>
2024-12-04 17:07:26 +0500

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,16 @@
 import { ThemedText } from '@/components/ThemedText';
 import * as SplashScreen from 'expo-splash-screen';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   return (
-    <View>
-      <ThemedText>Abdul</ThemedText>
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <ThemedText type='title' align='center' lightColor='green'>Abdul Ahad</ThemedText>
+      <ThemedText type='subtitle' align='center' lightColor='orange'>Abdul Ahad</ThemedText>
+      <ThemedText align='center'>Abdul Ahad</ThemedText>
     </View>
   );
 }
--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -21,12 +21,13 @@ export function ThemedText({
   return (
     <Text
       style={[
-        {color},
+        { color },
         type === 'default' ? styles.default : undefined,
         type === 'title' ? styles.title : undefined,
         type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
         type === 'subtitle' ? styles.subtitle : undefined,
         type === 'link' ? styles.link : undefined,
+        align && { textAlign: align },
         style,
       ]}
       {...rest}
